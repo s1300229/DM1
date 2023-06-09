1885d1cea84359e128239d0393bfd23125bba299
s1300229 <136040466+s1300229@users.noreply.github.com>
2023-06-09 12:19:21 +0900

    Won/lost message added

--- DiceGame.c
+++ DiceGame.c
@@ -14,5 +14,9 @@ printf("Dice 1: %d\n",dc1);
 printf("Dice 2: %d\n",dc2);
 printf("total value: %d\n",dc1 + dc2);
 
+  if(dc1 + dc2 > 7){
+printf("%You won!\n);
+       }
+       else printf("%You lost!\n");
 return 0;
 }
